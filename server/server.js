57fa1b0d7c330c7355737c55ec4ed10689ee7ab2
Adrian West <adrianwest@Adrians-MBP.home>
2017-03-21 13:30:28 +0000

    add connec t and disconnet event handlers

--- server/server.js
+++ server/server.js
@@ -1,16 +1,28 @@
 const path = require('path');  //this is a build in node module.  doesn't need installing
+const http = require('http');
+const express = require('express')
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '/../public');
-const express = require('express')
-const port = process.env.PORT || 3000;
-var app = express();
 
+const port = process.env.PORT || 3000;
+
+var app = express();
+var server = http.createServer(app);      //reson to use this instead of express build in http service is cos we want to use socket.io
+var io = socketIO(server);                //giving us 2 way comms between client and server.
 
 app.use(express.static(publicPath));
 
 
+io.on('connection', (socket) => {
+  console.log('new user connected');
 
-app.listen(port, () =>{
+  socket.on('disconnect', (socket) => {
+    console.log('User disconnected');
+  });
+});
+
+server.listen(port, () =>{
   console.log(`Listening on port: ${3000}`);
 });
 /////abiove. simple node servder serving up the static html file in /public
